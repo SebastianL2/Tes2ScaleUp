e314b270aef175965441214e353fa2a5e7aedd11
SebastianL2 <sebas16cely@gmail.com>
2024-04-18 03:24:01 -0500

    ll che

---
+++ config-medicare.php
@@ -0,0 +1,9 @@
+<?php
+$ipZip = "12345"; 
+$currentDomain = 'localhost';
+$domainName =' Golden Years ';
+$mbOfferSite = 'https://nationalcareoptions.com/lp/sca-aca';
+$rtkCmpId='646e2b29e54ddc000111d938';
+$ringbaScriptPrimary='//b-js.ringba.com/CAb49cfcb9181147da91c79a5b41b45130';
+$ringbaNumberPrimary='CALL (XXX) XXX-XXXX'
+?>
